7da14bea7d4b382beecee36c050d93aa1b5c6fb2
w1gz <w1gz@noreply.com>
2019-02-20 23:44:46 +0100

    [Haskell] RNA Transcription, without custom types

--- haskell/rna-transcription/src/DNA.hs
+++ haskell/rna-transcription/src/DNA.hs
@@ -1,37 +1,11 @@
-module DNA
-        ( toRNA
-        )
-where
+module DNA ( toRNA ) where
 
-data CommonNucleotide = Adenine | Cytosine | Guanine
-data DNA = DNA CommonNucleotide | Thymine
-data RNA = RNA CommonNucleotide | Uracil
-
-rnaToChar :: RNA -> Char
-rnaToChar rna = case rna of
-        RNA Adenine  -> 'A'
-        RNA Cytosine -> 'C'
-        RNA Guanine  -> 'G'
-        Uracil       -> 'U'
-
-dnaToRNA :: DNA -> RNA
-dnaToRNA dna = case dna of
-        DNA Guanine  -> RNA Cytosine
-        DNA Cytosine -> RNA Guanine
-        Thymine      -> RNA Adenine
-        DNA Adenine  -> Uracil
-
-charToDNA :: Char -> Maybe DNA
-charToDNA c | c == 'A'  = Just $ DNA Adenine
-            | c == 'C'  = Just $ DNA Cytosine
-            | c == 'G'  = Just $ DNA Guanine
-            | c == 'T'  = Just Thymine
-            | otherwise = Nothing
-
-strandComplement :: Char -> Either Char Char
-strandComplement c = case charToDNA c of
-        Just dna -> Right $ rnaToChar . dnaToRNA $ dna
-        Nothing  -> Left c
+charToRNA :: Char -> Either Char Char
+charToRNA char | char == 'A' = Right 'U'
+               | char == 'C' = Right 'G'
+               | char == 'G' = Right 'C'
+               | char == 'T' = Right 'A'
+               | otherwise   = Left char
 
 toRNA :: String -> Either Char String
-toRNA = traverse strandComplement
+toRNA = traverse charToRNA
